c19b8e592fb9d616a40cc93ff23de046da3b617d
Annie <anniehartmann97@gmail.com>
2023-02-10 12:53:47 +0300

    Locomotive + GSAP

--- src/assets/js/modules/Animation.js
+++ src/assets/js/modules/Animation.js
@@ -1,14 +1,47 @@
 import LocomotiveScroll from 'locomotive-scroll';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 class Animation {
 	constructor() {
 		this.init();
 	}
 	init() {
-		new LocomotiveScroll({
-			el: document.querySelector('[data-scroll-container]'),
+		const locoScroll = new LocomotiveScroll({
+			el: document.querySelector('.smooth-scroll'),
 			smooth: true,
 		});
+
+		locoScroll.on('scroll', ScrollTrigger.update);
+
+		ScrollTrigger.scrollerProxy('.smooth-scroll', {
+			scrollTop(value) {
+				return arguments.length
+					? locoScroll.scrollTo(value, 0, 0)
+					: locoScroll.scroll.instance.scroll.y;
+			},
+			getBoundingClientRect() {
+				return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+			},
+			pinType: document.querySelector('.smooth-scroll').style.transform ? 'transform' : 'fixed',
+		});
+
+		gsap.fromTo(
+			'.hero-section',
+			{ opacity: 1 },
+			{
+				opacity: 0,
+				scrollTrigger: {
+					trigger: '.hero-section',
+					scroller: '.smooth-scroll',
+					markers: true,
+					start: 'center',
+					end: 'bottom',
+					scrub: true,
+				},
+			}
+		);
 	}
 }
 
