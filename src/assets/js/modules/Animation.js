b501f1c971439ad392e9cdc1d6bd7d3273a0ead4
Annie <anniehartmann97@gmail.com>
2023-02-10 14:04:47 +0300

    Lesson done :tada:

--- src/assets/js/modules/Animation.js
+++ src/assets/js/modules/Animation.js
@@ -6,6 +6,13 @@ gsap.registerPlugin(ScrollTrigger);
 class Animation {
 	constructor() {
 		this.init();
+		this.getUserScreen();
+		window.addEventListener('resize', () => this.getUserScreen());
+	}
+	getUserScreen() {
+		const userScreen = window.innerWidth;
+		const elementUserScreen = document.querySelector('[data-user-screen]');
+		elementUserScreen.innerHTML = userScreen;
 	}
 	init() {
 		const locoScroll = new LocomotiveScroll({
@@ -35,13 +42,53 @@ class Animation {
 				scrollTrigger: {
 					trigger: '.hero-section',
 					scroller: '.smooth-scroll',
-					markers: true,
-					start: 'center',
+					start: '+=30%',
 					end: 'bottom',
 					scrub: true,
 				},
 			}
 		);
+
+		const itemsL = gsap.utils.toArray('.gallery__left .gallery__item');
+		itemsL.forEach(item => {
+			gsap.fromTo(
+				item,
+				{
+					x: -150,
+					opacity: 0,
+				},
+				{
+					opacity: 1,
+					x: 0,
+					scrollTrigger: {
+						trigger: item,
+						scroller: '.smooth-scroll',
+						scrub: true,
+					},
+				}
+			);
+		});
+
+		const itemsR = gsap.utils.toArray('.gallery__right .gallery__item');
+		itemsR.forEach(item => {
+			gsap.fromTo(
+				item,
+				{
+					x: 100,
+					opacity: 0,
+				},
+				{
+					opacity: 1,
+					x: 0,
+					scrollTrigger: {
+						trigger: item,
+						scroller: '.smooth-scroll',
+						scrub: true,
+						start: 100,
+					},
+				}
+			);
+		});
 	}
 }
 
